142cf50f1e3a8432e8bb9732fa7e739fc307aa55
Imogen <me@imogen.tech>
2024-07-23 17:08:10 +0200

    update to use new data file layout

--- src/main.js
+++ src/main.js
@@ -1,104 +1,76 @@
-/** @type RegExp */
-var groupRegex;
-var groupColumn;
-chrome.storage.sync.get(["groupPattern", "groupColumn"], (items) => {
-  groupRegex = new RegExp(items.groupPattern, "g");
-  groupColumn = items.groupColumn;
-});
-
-chrome.storage.sync.onChanged.addListener((changes) => {
-  if (changes.groupPattern?.newValue) {
-    groupRegex = new RegExp(changes.groupPattern.newValue, "g");
-  }
-  if (changes.groupColumn?.newValue) {
-    groupColumn = changes.groupColumn.newValue;
-  }
-});
-
-async function getFileContent() {
-  const inputElem = document.createElement("input");
-  inputElem.type = "file";
-  var resolve;
-  const promise = new Promise((res, _) => {
-    resolve = res;
-  });
-  inputElem.onchange = (e) => {
-    resolve();
-  };
-
-  inputElem.click();
-  await promise;
-  return await inputElem.files[0].text();
-}
-
-/**
- * Parses a csv into a js object
- * @param {string} file
- * @returns {object} object containing the parsed values as Map<string, [number, string]>
- */
-function parseCSV(file) {
-  return Object.fromEntries(
-    file
-      .split(/\r?\n/)
-      .filter((line) => line.length !== 0)
-      .map((line) => {
-        const [group, points, ...comment] = line.split(",").map((s) => s.trim());
-        switch (comment.length) {
-          case 0:
-            return [group, [Number(points), ""]];
-          case 1:
-            return [group, [Number(points), comment[0]]];
-          default:
-            throw "incorrectly formatted file";
-        }
-      })
-  );
-}
-
 /**
  *
  * @param {HTMLTableRowElement[]} table
- * @param {{ [key: string]: [number, string] }} marks
+ * @param {{
+ *   identifier_column: string;
+ *   data: {
+ *     [key: string]: {
+ *       points: Number | null;
+ *       feedback?: string;
+ *     }
+ *   }
+ * }} student_data
  * @returns
  */
-function fillInMarks(table, marks) {
+function fillInGrades(table, student_data) {
   var overriden = false;
   const columns = [...table.querySelectorAll("thead th")];
-  const groupColIndex = columns.indexOf(columns.find((c) => c.innerText.includes(groupColumn)));
+  const groupColIndex = columns.indexOf(
+    columns.find((c) => c.innerText.includes(student_data.identifier_column))
+  );
 
   for (const row of table.querySelectorAll("tbody tr")) {
     const groupElem = row.children[groupColIndex];
     const inputElem = row.querySelector("input.quickgrade");
     const feedbackElem = row.querySelector("textarea.quickgrade");
-    if (groupElem === undefined || groupElem.textContent === null || inputElem === undefined) {
+    if (!groupElem?.textContent || !inputElem) {
       continue;
     }
-    const matches = [...groupElem.textContent.matchAll(groupRegex)];
-    if (matches.length === 0) {
-      continue;
-    }
-    const groupNum = matches[0][1];
-    if (!(groupNum in marks)) {
+    const curr_data = student_data.data[groupElem.textContent];
+    if (!curr_data) {
       continue;
     }
     if (
-      (inputElem.value !== "" && Number(inputElem.value) !== marks[groupNum][0]) ||
-      (feedbackElem && feedbackElem.value !== "" && feedbackElem.value !== marks[groupNum][1])
+      (inputElem.value !== "" && Number(inputElem.value) !== curr_data.points) ||
+      (feedbackElem && feedbackElem.value !== "" && feedbackElem.value !== curr_data.feedback)
     ) {
       row.classList.remove("unselectedrow");
       row.classList.add("selectedrow");
       overriden = true;
     }
-    if (inputElem.value === "" || Number(inputElem.value) !== marks[groupNum][0]) {
+    if (inputElem.value === "" || Number(inputElem.value) !== curr_data.points) {
       inputElem.parentElement.classList.add("quickgrademodified");
-      inputElem.value = marks[groupNum][0];
+      inputElem.value = curr_data.points;
     }
-    if (feedbackElem && feedbackElem.value !== marks[groupNum][1]) {
+    if (feedbackElem && feedbackElem.value !== curr_data.feedback) {
       feedbackElem.parentElement.classList.add("quickgrademodified");
-      feedbackElem.value = marks[groupNum][1];
+      feedbackElem.value = curr_data.feedback;
     }
   }
   return overriden;
+}
+
+/**
+ *
+ * @param {Event} event
+ * @returns
+ */
+async function fileSelect(event) {
+  if (!event.target?.files || event.target.files[0] === undefined) {
+    return;
+  }
+  const text = await files[0].text();
+  const student_data = await JSON.parse(text);
+
+  const didOverwrite = fillInMarks(gradingTable, student_data);
+  messageElem.textContent =
+    "Imported data, double check it's correct and submit with the button below.";
+  if (didOverwrite) {
+    messageElem.textContent += " This overwrote some data, affected rows are highlighted.";
+  }
+  messageElem.hidden = false;
+  inputElem.files = null;
+  inputElem.value = "";
 }
 
 const gradingForms = document.getElementsByClassName("quickgradingform");
@@ -112,20 +84,7 @@ if (gradingForms.length !== 0) {
   const inputElem = document.createElement("input");
   inputElem.type = "file";
   inputElem.style.display = "none";
-  inputElem.onchange = async () => {
-    if (!inputElem.files || inputElem.files[0] === undefined) {
-      return;
-    }
-    const didOverwrite = fillInMarks(gradingTable, parseCSV(await inputElem.files[0].text()));
-    messageElem.textContent =
-      "Imported data, double check it's correct and submit with the button below.";
-    if (didOverwrite) {
-      messageElem.textContent += " This ovverwrote some data, affected rows are highlighted.";
-    }
-    messageElem.hidden = false;
-    inputElem.files = null;
-    inputElem.value = "";
-  };
+  inputElem.onchange = fileSelect;
   const inputButton = document.createElement("button");
   inputButton.classList.add("btn", "btn-primary");
   inputButton.innerText = "Import from file";
